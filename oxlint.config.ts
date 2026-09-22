aba36250303baab88a311e94c425edfb8bdbdd2c
Aidan Timson <aidan@timmo.dev>
2026-09-22 10:37:55 +0100

    Enable type-aware Oxlint checks

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -3,4 +3,7 @@ import recommendedEffect from "@timmo001/oxlint-rules/configs/recommended-effect
 export default {
   extends: [recommendedEffect],
   ignorePatterns: ["dist/**"],
+  options: {
+    typeAware: true,
+  },
 };
